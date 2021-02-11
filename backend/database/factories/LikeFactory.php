8f59738443a6115e15e7bb707a40fef9ceee007b
danielsimonney <daniel.simonney60@gmail.com>
2021-02-11 11:52:13 +0100

    ad

--- backend/database/factories/LikeFactory.php
+++ backend/database/factories/LikeFactory.php
@@ -4,26 +4,28 @@ namespace Database\Factories;
 
 use App\Models\Like;
 use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class LikeFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Like::class;
+  /**
+   * The name of the factory's corresponding model.
+   *
+   * @var string
+   */
+  protected $model = Like::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            "is_liked" => $this->faker->boolean()
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array
+   */
+  public function definition()
+  {
+    return [
+      "is_liked" => $this->faker->boolean(),
+      'user_id' => User::factory()
+    ];
+  }
 }
