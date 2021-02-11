1f98220111049e0e5e1025343e6546fc934d46d2
snarcraft <snarcraft@yahoo.fr>
2021-02-11 15:48:10 +0100

    feat(app): add seeders assignment

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,35 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\Comment;
-use App\Models\Post;
-use App\Models\Tag;
-use App\Models\Topic;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
-     * @return void
      */
     public function run()
     {
-        for ($i = 0; $i < 10; $i++) {
-            $user = User::factory()->create();
-            $user2 = User::factory()->create();
-            $tag = Tag::factory()->create();
-            $topic = Topic::factory()->for($user)->hasAttached($tag)->create();
-            $post = Post::factory()
-                ->for($user2)
-                ->for($topic)
-                ->create();
-            $comment = Comment::factory()->for($user)->for(
-                $post,
-                'commentable'
-            )->create();
-        }
+        // 1) Create env variables to contain the number of Model that you want to seed in the .env
+        // SEED_USERS_COUNT = 10
+        // SEED_POSTS_COUNT = 10
+        // ...
+
+        // 2) Then you need to create a configuration file in config/name_of_the_file.php
+
+        // 3) And then you need to create separately those items using the factory
+
+        // 4) Finally to need to chain all the factories to create all the items
     }
 }
